51ab25faec1c81fa34211eeda7c73f3ac1fc20e1
blinovmaksym <m.blinov@it-sprout.org>
2023-07-06 11:53:24 +0300

    first commit

--- rxresume/scripts/create-s3.sh
+++ rxresume/scripts/create-s3.sh
@@ -2,11 +2,11 @@
 
 bucket_name="rxresume3bucketmax"
 
-# aws s3api head-bucket --bucket "$bucket_name"  2>/dev/null
+aws s3api head-bucket --bucket "$bucket_name"  2>/dev/null
 
-# if [[ $? -eq 0 ]]; then
-#   echo "Bucket $bucket_name alredy exist."
-# else
-aws s3api create-bucket --bucket "$bucket_name" 
-echo "Bucket $bucket_name created successfully."
-# fi
+if [[ $? -eq 0 ]]; then
+  echo "Bucket $bucket_name alredy exist."
+else
+  aws s3api create-bucket --bucket "$bucket_name" 
+  echo "Bucket $bucket_name created successfully."
+fi
