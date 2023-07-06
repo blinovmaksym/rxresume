3fd55bae029ce269f862f671fe8d168397fa43e3
blinovmaksym <m.blinov@it-sprout.org>
2023-07-06 18:23:34 +0300

    test

--- rxresume/main.tf
+++ rxresume/main.tf
@@ -28,7 +28,7 @@ resource "aws_vpc" "rxresume-vpc" {
 # }
 resource "aws_subnet" "public_subnet" {
 vpc_id = aws_vpc.rxresume-vpc.id
-  cidr_block              = "10.0.1.0/16"
+  cidr_block              = "10.0.1.0/24"
   availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
 
