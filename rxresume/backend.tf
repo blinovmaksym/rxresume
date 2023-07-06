6ff8b998d90ec524dd6b823da9fdb65c000277cd
blinovmaksym <m.blinov@it-sprout.org>
2023-07-06 11:29:05 +0300

    first commit

--- rxresume/backend.tf
+++ rxresume/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "rxresume3bucket"  
+    bucket         = "rxresume3bucket_max"  
     key            = "terraform.tfstate"   
     region         = "us-east-1"           
     encrypt        = true             
