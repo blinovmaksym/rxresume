a2f434092e1c81c58bcb4052809a90124397f5d2
blinovmaksym <m.blinov@it-sprout.org>
2023-07-06 14:15:37 +0300

    test

--- rxresume/main.tf
+++ rxresume/main.tf
@@ -17,13 +17,13 @@ module "vpc" {
   cidr                 = "172.16.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
-  public_subnets       = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+  public_subnets       = ["172.16.4.0/24", "172.16.5.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
 }
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = module.vpc.vpc_id
-  cidr_block              = "172.16.4.0/24"
+  cidr_block              = "172.16.6.0/24"
   availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
 }
