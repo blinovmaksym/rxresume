92b7aecb542268ade04f1033a442fc9f66b8b305
Maksym Blinov <m.blinov@it-sprout.org>
2023-07-06 23:19:09 +0300

    destroy

--- rxresume/main.tf
+++ rxresume/main.tf
@@ -169,7 +169,7 @@ resource "aws_db_instance" "rds_instance" {
   publicly_accessible = false
   db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
   vpc_security_group_ids = [aws_security_group.rxresume-sg-db.id]
-  skip_final_snapshot  = true
+  skip_final_snapshot  = false
 
   tags = {
     Name = "rds-instance"
