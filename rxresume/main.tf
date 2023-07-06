c3d5804ef8f9aceefdbfda45449716e73c9956b0
blinovmaksym <m.blinov@it-sprout.org>
2023-07-06 13:19:00 +0300

    first commit

--- rxresume/main.tf
+++ rxresume/main.tf
@@ -18,6 +18,6 @@ resource "aws_instance" "ec2_instance" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.small"
   key_name      = aws_key_pair.key_pair.key_name
-  vpc_id               = aws_vpc.my_vpc.id
+  vpc_id               = aws_vpc.my_vpc.vpc_id
 }
 
