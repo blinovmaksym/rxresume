c6caea402f147e32ad46ef6fbe29344f9ea9a338
blinovmaksym <m.blinov@it-sprout.org>
2023-07-06 21:40:44 +0300

    test

--- rxresume/main.tf
+++ rxresume/main.tf
@@ -18,7 +18,7 @@ resource "aws_vpc" "rxresume-vpc" {
 resource "aws_subnet" "public_subnet" {
 vpc_id = aws_vpc.rxresume-vpc.id
   cidr_block              = "10.0.1.0/24"
-
+  availability_zone       = data.aws_availability_zones.available.names[0]
   tags = {
     Name = "Public-subnet"
   }
@@ -27,6 +27,9 @@ vpc_id = aws_vpc.rxresume-vpc.id
 resource "aws_subnet" "back-end-net" {
   vpc_id     = aws_vpc.rxresume-vpc.id
   cidr_block = "10.0.2.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[1]  # Используйте нужную доступную зону
+
+
   tags = {
     Name = "Private-subnet"
   }
