d5c1d9f864ce5a73b01c8e152812a9305725f0c9
blinovmaksym <m.blinov@it-sprout.org>
2023-07-06 18:13:43 +0300

    test

--- rxresume/main.tf
+++ rxresume/main.tf
@@ -7,20 +7,25 @@ resource "aws_key_pair" "key_pair" {
   key_name   = "ssh_key_resume"
   public_key = var.SSH_KEY
 }
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.0.0"
-
-
-  name                 = "rxresume-vpc"
-  cidr                 = "172.16.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["172.16.1.0/24", "172.16.2.0/24"]
-  public_subnets       = ["172.16.3.0/24", "172.16.4.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+resource "aws_vpc" "rxresume-vpc" {
+     cidr_block = "10.0.0.0/16"
+      tags = {
+        Name = "rxresume-net"
+  }
 }
+# module "vpc" {
+#   source  = "terraform-aws-modules/vpc/aws"
+#   version = "5.0.0"
+
+
+#   name                 = "rxresume-vpc"
+#   cidr                 = "172.16.0.0/16"
+#   azs                  = data.aws_availability_zones.available.names
+#   private_subnets      = ["172.16.1.0/24", "172.16.2.0/24"]
+#   public_subnets       = ["172.16.3.0/24", "172.16.4.0/24"]
+#   enable_nat_gateway   = true
+#   single_nat_gateway   = true
+# }
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = module.vpc.vpc_id
   cidr_block              = "172.16.5.0/24"
@@ -33,7 +38,7 @@ resource "aws_subnet" "public_subnet" {
 }
 
 resource "aws_internet_gateway" "rxresume-GW" {
-  vpc_id = module.vpc.vpc_id
+  vpc_id = aws_vpc.rxresume-vpc.id
 
   tags = {
     Name = "rxresume-GW"
@@ -41,7 +46,7 @@ resource "aws_internet_gateway" "rxresume-GW" {
 }
 
 resource "aws_route_table" "rxresume-RT" {
-  vpc_id = module.vpc.vpc_id
+  vpc_id = aws_vpc.rxresume-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -61,7 +66,7 @@ resource "aws_route_table_association" "a-front-net" {
 
 resource "aws_security_group" "ingress-all-test" {
 name = "allow-all-sg"
-vpc_id = module.vpc.vpc_id
+vpc_id = aws_vpc.rxresume-vpc.id
 ingress {
   description      = "SSH from VPC"
     cidr_blocks = [
