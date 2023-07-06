310f75a8206de7372314430c38b8b5dfcb9be342
blinovmaksym <m.blinov@it-sprout.org>
2023-07-06 13:52:42 +0300

    first commit

--- rxresume/main.tf
+++ rxresume/main.tf
@@ -30,4 +30,27 @@ resource "aws_instance" "ec2_instance" {
   vpc_security_group_ids = [module.vpc.default_security_group_id]
   subnet_id     = module.vpc.public_subnets[0]
 }
+# Создание балансировщика нагрузки
+resource "aws_lb" "load_balancer" {
+  name               = "rxresume-load-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [module.vpc.default_security_group_id]
+  subnets            = module.vpc.public_subnets
+}
 
+# Привязка инстанса EC2 к балансировщику нагрузки
+resource "aws_lb_target_group_attachment" "ec2_attachment" {
+  target_group_arn = aws_lb.load_balancer.arn
+  target_id        = aws_instance.ec2_instance.id
+  port             = 80
+}
+# Обновление безопасной группы для разрешения доступа по SSH
+resource "aws_security_group_rule" "ssh_rule" {
+  security_group_id = module.vpc.default_security_group_id
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
