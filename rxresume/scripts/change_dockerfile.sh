b8771569b38e42563e629223ee8353c21ab417f4
blinovmaksym <m.blinov@it-sprout.org>
2023-07-07 20:15:10 +0300

    init

--- rxresume/scripts/change_dockerfile.sh
+++ rxresume/scripts/change_dockerfile.sh
@@ -6,8 +6,8 @@ PUBLIC_SERVER_URL="http://example.com"
 
 
 # Изменить значения полей в docker-compose.yml
-sed -i "s|PUBLIC_URL=.*|PUBLIC_URL=$PUBLIC_URL|" docker-compose.yml
-sed -i "s|PUBLIC_SERVER_URL=.*|PUBLIC_SERVER_URL=$PUBLIC_SERVER_URL|" docker-compose.yml
+sed -i "s|PUBLIC_URL=.*|PUBLIC_URL=$PUBLIC_URL|" ../app/docker-compose.yml
+sed -i "s|PUBLIC_SERVER_URL=.*|PUBLIC_SERVER_URL=$PUBLIC_SERVER_URL|" ../app/docker-compose.yml
 
 
 
