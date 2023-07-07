ef69928cecf5a8563d5c944d09ee8c0d86159b2a
blinovmaksym <m.blinov@it-sprout.org>
2023-07-07 16:01:09 +0300

    test

--- rxresume/scripts/install.sh
+++ rxresume/scripts/install.sh
@@ -2,7 +2,7 @@
 sudo apt update && sudo apt upgrade -y
 
 # Install Docker
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt install -y docker-ce
