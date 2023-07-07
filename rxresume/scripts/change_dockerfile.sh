9a62a6c9c82fe980927ceda73e080571808b081f
blinovmaksym <m.blinov@it-sprout.org>
2023-07-07 22:41:28 +0300

    init

--- rxresume/scripts/change_dockerfile.sh
+++ rxresume/scripts/change_dockerfile.sh
@@ -3,6 +3,7 @@
 # Изменить значения полей в docker-compose.yml
 sed -i "s|PUBLIC_URL=.*|PUBLIC_URL=http://$PUBLIC_URL:3000|" ../app/docker-compose.yml
 sed -i "s|PUBLIC_SERVER_URL=.*|PUBLIC_SERVER_URL=http://$PUBLIC_URL:3100|" ../app/docker-compose.yml
+sed -i "s|POSTGRES_HOST=.*|POSTGRES_HOST=$POSTGRES_HOST|" ../app/docker-compose.yml
 
 
 
