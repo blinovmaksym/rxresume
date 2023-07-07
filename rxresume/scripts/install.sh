12eaf8e477743bd8693b683f1ca18836a7b05604
blinovmaksym <m.blinov@it-sprout.org>
2023-07-07 20:07:29 +0300

    init

--- rxresume/scripts/install.sh
+++ rxresume/scripts/install.sh
@@ -19,7 +19,6 @@ sudo chown $USER /var/run/docker.sock
 # Verify that Docker Compose is installed
 docker compose version
 
-cd ../.
 mkdir app && cd app
 curl -L https://raw.githubusercontent.com/blinovmaksym/rxresume/main/docker-compose.yml > docker-compose.yml
 curl -L https://raw.githubusercontent.com/AmruthPillai/Reactive-Resume/main/.env.example > .env
