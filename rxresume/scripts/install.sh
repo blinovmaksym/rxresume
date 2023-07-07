d3e293edf36abd378b1fa652b29d481796cf8b3e
blinovmaksym <m.blinov@it-sprout.org>
2023-07-07 18:50:00 +0300

    test

--- rxresume/scripts/install.sh
+++ rxresume/scripts/install.sh
@@ -11,17 +11,17 @@ sudo apt install -y docker-ce
 # sudo systemctl status docker
 
 # Install Docker Compose
-mkdir -p ~/.docker/cli-plugins/
-curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-chmod +x ~/.docker/cli-plugins/docker-compose
+sudo mkdir -p ~/.docker/cli-plugins/
+sudocurl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+sudo chmod +x ~/.docker/cli-plugins/docker-compose
 sudo chown $USER /var/run/docker.sock
 
 # Verify that Docker Compose is installed
 docker compose version
 
 cd ../.
-mkdir app && cd app
-curl -L https://raw.githubusercontent.com/blinovmaksym/rxresume/main/docker-compose.yml > docker-compose.yml
-curl -L https://raw.githubusercontent.com/AmruthPillai/Reactive-Resume/main/.env.example > .env
+sudo mkdir app && cd app
+sudo curl -L https://raw.githubusercontent.com/blinovmaksym/rxresume/main/docker-compose.yml > docker-compose.yml
+sudo curl -L https://raw.githubusercontent.com/AmruthPillai/Reactive-Resume/main/.env.example > .env
 
 docker compose up
