8dd0fa7f393521a06e272af29a0be7b1ef862c2b
blinovmaksym <m.blinov@it-sprout.org>
2023-07-08 11:39:38 +0300

    init

--- rxresume/scripts/install.sh
+++ rxresume/scripts/install.sh
@@ -20,25 +20,15 @@ sudo chown $USER /var/run/docker.sock
 docker compose version
 
 
-if [ -d "app" ]; then
-  cd "app"
-else
-  mkdir "app"
-  cd "app"
-fi
+mkdir "app" &&  cd "app"
+
 curl -L https://raw.githubusercontent.com/blinovmaksym/rxresume/main/docker-compose.yml > docker-compose.yml
 curl -L https://raw.githubusercontent.com/AmruthPillai/Reactive-Resume/main/.env.example > .env
 
-if [ -d "scripts" ]; then
-  cd "scripts"
-  echo "Переход в папку 'scripts' выполнен"
+cd "scripts"
+chmod +x change_dockerfile.sh
+./change_dockerfile.sh
 
-  chmod +x change_dockerfile.sh
-  echo "Установлены права выполнения для 'change_dockerfile.sh'"
-
-  ./change_dockerfile.sh
-  echo "Выполнен скрипт 'change_dockerfile.sh'"
-fi
 
 # Выполнение команды в фоновом режиме
 docker compose up -d &
