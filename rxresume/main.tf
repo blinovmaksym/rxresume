0908d33532221a76872bd0ac2a652b43c01e1f78
blinovmaksym <m.blinov@it-sprout.org>
2023-07-08 10:44:10 +0300

    init

--- rxresume/main.tf
+++ rxresume/main.tf
@@ -142,11 +142,11 @@ resource "aws_db_instance" "rds_instance" {
   engine_version       = "14.7"
   instance_class       = "db.t3.micro"
   allocated_storage    = 20
-  db_name              = "mydb"
+  db_name              = "postgres"
   storage_type         = "gp2"
   identifier           = "my-rds-instance"
-  username             = "myuser"
-  password             = "mypassword"
+  username             = "postgres"
+  password             = "postgres"
   publicly_accessible = false
   db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
   vpc_security_group_ids = [aws_security_group.rxresume-sg.id]
