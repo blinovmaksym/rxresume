e3de5f1d48c33f144a8fb5d5dc9a230e0836b2d9
blinovmaksym <m.blinov@it-sprout.org>
2023-07-09 20:08:54 +0300

    init

--- rxresume/main.tf
+++ rxresume/main.tf
@@ -19,7 +19,7 @@ resource "aws_vpc" "rxresume-vpc" {
 resource "aws_subnet" "public_subnet" {
 vpc_id = aws_vpc.rxresume-vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = data.aws_availability_zones.available.names[0]
+  availability_zone       = [data.aws_availability_zones.available.names[0],data.aws_availability_zones.available.names[3]]
   tags = {
     Name = "Public-subnet"
   }
