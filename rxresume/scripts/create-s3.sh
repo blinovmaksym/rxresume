1890fb1892b847d7f44f74aff383ecc5d8a605bc
blinovmaksym <m.blinov@it-sprout.org>
2023-07-10 18:11:49 +0300

    init

--- rxresume/scripts/create-s3.sh
+++ rxresume/scripts/create-s3.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-bucket_name="rxresume3bucketmax1"
+bucket_name="rxresume3bucket"
 
-aws s3api head-bucket --bucket "$bucket_name"  2>/dev/null
+aws s3api head-bucket --bucket "$bucket_name" --region us-east-2 2>/dev/null
 
 if [[ $? -eq 0 ]]; then
   echo "Bucket $bucket_name alredy exist."
 else
-  aws s3api create-bucket --bucket "$bucket_name" 
+  aws s3api create-bucket --region us-east-2 --bucket "$bucket_name" 
   echo "Bucket $bucket_name created successfully."
 fi
 
