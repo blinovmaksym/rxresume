58296ef604c9696cb601e5e1355c7329fb8d51f8
blinovmaksym <m.blinov@it-sprout.org>
2023-07-11 09:42:53 +0300

    test

--- rxresume/lb.tf
+++ rxresume/lb.tf
@@ -25,23 +25,23 @@ resource "aws_lb_listener" "http" {
   }
 }
 
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.rxresume-alb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
+# resource "aws_lb_listener" "https" {
+#   load_balancer_arn = aws_lb.rxresume-alb.arn
+#   port              = 443
+#   protocol          = "HTTPS"
+#   ssl_policy        = "ELBSecurityPolicy-2016-08"
 
-  certificate_arn = "<YOUR_SSL_CERTIFICATE_ARN>"
+#   certificate_arn = "<YOUR_SSL_CERTIFICATE_ARN>"
 
-  default_action {
-    type             = "redirect"
-    redirect {
-      port       = "3000"
-      protocol   = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
+#   default_action {
+#     type             = "redirect"
+#     redirect {
+#       port       = "3000"
+#       protocol   = "HTTPS"
+#       status_code = "HTTP_301"
+#     }
+#   }
+# }
 resource "aws_lb_target_group" "rxresume-tg" {
   name     = "rxresume-tg"
   port     = 3000
@@ -69,15 +69,15 @@ resource "aws_lb_listener_rule" "http_redirect" {
   }
 }
 
-resource "aws_lb_listener_rule" "https_redirect" {
-  listener_arn = aws_lb_listener.https.arn
-  priority     = 1
+# resource "aws_lb_listener_rule" "https_redirect" {
+#   listener_arn = aws_lb_listener.https.arn
+#   priority     = 1
 
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.rxresume-tg.arn
-  }
-}
+#   action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.rxresume-tg.arn
+#   }
+# }
 
 resource "aws_lb_target_group_attachment" "rxresume-tg-attachment" {
   target_group_arn = aws_lb_target_group.rxresume-tg.arn
